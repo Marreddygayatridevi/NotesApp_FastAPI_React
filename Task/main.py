c341fbca8525915fbfde19a60552edddbce37a80
Marreddygayatridevi <marreddygayatri@gmail.com>
2025-04-16 14:14:02 +0530

    initial commit

---
+++ Task/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from auth import router as auth_router
+from Notes import router as notes_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth_router)
+app.include_router(notes_router, tags=["Notes"])
